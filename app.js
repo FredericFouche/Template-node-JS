cf0ab31b75a6a393adfae3523042f880be34086b
Frédéric Fouché <itsme@fredericfouche.fr>
2023-12-12 18:04:17 +0100

    add dotenv/gestion du port

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 //configuration de base du serveur express
-
+//import de dotenv
+require('dotenv').config();
 //importation des modules
-const express = require("express");
-const path = require("path");
-const utilsModule = require("./app/utils");
+const express = require('express');
+const path = require('path');
+const utilsModule = require('./app/utils');
 
 //importation du router
-const router = require("./app/router");
+const router = require('./app/router');
 
 // Créer une app express
 const app = express();
 
 //configuration ejs
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/app/views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/app/views'));
 
 //configuration du dossier static
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //Enregistrement des locals - pour fournir des données communes à chaque vue
 app.use(utilsModule.setLocals);
@@ -24,7 +25,7 @@ app.use(utilsModule.setLocals);
 app.use(router);
 
 //définition du port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //lancement du server
 app.listen(PORT, () => {
